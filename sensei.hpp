455fde0ca5c6dc525c3abe5847e933615493670e
curiousleo <curiousleo@ymail.com>
2011-08-23 22:42:06 +0100

    sensei.hpp: Added test Sudoku string, "Value" typedef, "read", "display", "solve" prototypes

--- sensei.hpp
+++ sensei.hpp
@@ -1,14 +1,21 @@
 #ifndef GUARD_sensei_h
 #define GUARD_sensei_h
 
+
+#define SUDOKU1 "...6....445....2......893..97......3.63........4..27..6.9.5.............5....3.61"
+
 /* A Sudoku is a vector of unsigned chars (Values).
  * The vector contains the remaining possible values for the corresponding
  * cell.
  */
 typedef unsigned char Position;
-typedef unsigned char Values;
-typedef std::vector<Values, std::allocator<Position>> Sudoku;
+typedef unsigned char Value;
+typedef std::set<Value> Values;
+typedef std::vector<Values, std::allocator<Position> > Sudoku;
 
 void init(void);
+void read(Sudoku&);
+void display(Sudoku);
+void solve(Sudoku&);
 
 #endif // GUARD_sensei_h guard
