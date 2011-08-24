fa25cc690c7e4b35b69a9d51c3c3e29a3e3f75c2
curiousleo <curiousleo@ymail.com>
2011-08-24 22:08:02 +0100

    optimize.sh: Added -lboost_thread-mt compiler option

--- bin/optimize.sh
+++ bin/optimize.sh
@@ -1,4 +1,4 @@
 for opt in "-O0" "-O1" "-O2" "-O3"
 do
-	echo "Optimization ${opt}"; g++ ${opt} -std=c++0x -o sensei ../src/sensei.cpp && du -h sensei && ./timefile.sh ../test/*; echo
+	echo "Optimization ${opt}"; g++ -lboost_thread-mt ${opt} -std=c++0x -o sensei ../src/sensei.cpp && du -h sensei && ./timefile.sh ../test/*; echo
 done
