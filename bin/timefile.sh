e3f53c41df20dabb9c49bfbd6b4b1985857dfe90
curiousleo <curiousleo@ymail.com>
2011-08-24 22:39:44 +0100

    timefile.sh: Redirect sensei output to /dev/null

--- bin/timefile.sh
+++ bin/timefile.sh
@@ -7,7 +7,7 @@ do
 	echo -n "${infile}: "
 
 	exec 3>&1 4>&2
-   TIME=$( { time ./solvefile.sh ${infile} 1>&3 2>&4; } 2>&1 )  # Captures time only.
+   TIME=$( { time ./solvefile.sh ${infile} 1>/dev/null 2>/dev/null; } 2>&1 )  # Captures time only.
    exec 3>&- 4>&-
 
 	LENGTH=$(wc -l ${infile} | cut -f1 -d' ')
