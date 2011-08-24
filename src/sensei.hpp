a90c72fd0547a31091dc016a905ba2014fba867b
curiousleo <curiousleo@ymail.com>
2011-08-24 22:56:23 +0100

    sensei.hpp: Added comments, "display" called by reference instead of value

--- src/sensei.hpp
+++ src/sensei.hpp
@@ -1,19 +1,21 @@
 #ifndef GUARD_sensei_h
 #define GUARD_sensei_h
 
-
-/* A Sudoku is a vector of unsigned chars (Values).
- * The vector contains the remaining possible values for the corresponding
+/* A Sudoku is a vector of sets of unsigned chars (Values).
+ * The set contains the remaining possible values for the corresponding
  * cell.
  */
+
+// Typedefs
 typedef unsigned char Position;
 typedef unsigned char Value;
 typedef std::set<Value> Values;
 typedef std::vector<Values, std::allocator<Position> > Sudoku;
 
+// Prototypes
 void init(void);
 void read(Sudoku&, const std::string);
-void display(const Sudoku);
+void display(const Sudoku&);
 bool solve(Sudoku&);
 
 void assign(Sudoku&, Position, Value);
