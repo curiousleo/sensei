98ae2ac87dac0806d7f5db555520727081e8f033
curiousleo <curiousleo@ymail.com>
2011-08-31 07:37:41 +0100

    cover.cpp: Includes <vector>; Added dummy function bodies for add_rows & add_row

--- src/cover.cpp
+++ src/cover.cpp
@@ -1,21 +1,27 @@
+#include <vector>
+
 #include "cover.hpp"
 
 ExactCover::ExactCover() {};
 
-ExactCover::add_row(std::array cont) {
-	typedef typename std::array::size_type array_sz;
-
-	root = &cont[0][0];
-	array_sz col_i_start = 1;
-
-	for (array_sz row_i = 0; row_i != cont.size(); ++row_i) {
-
-		for (
-				array_sz col_i = col_i_start;
-				col_i != cont[row_i].size(); ++col_i) {
-			;
-		}
-
-		col_i_start = 0;
+void ExactCover::add_rows(const std::vector<std::vector<bool> > *rows) {
+	int row_id = 0;
+	std::vector<std::vector<bool> >::const_iterator row_it;
+	
+	// Add each row
+	for (row_it = rows->begin(); row_it != rows->end(); ++row_it) {
+		add_row(&(*row_it), row_id);
+		++row_id;
 	}
 }
+
+void ExactCover::add_row(const std::vector<bool> *row, int row_id) {
+	std::vector<bool>::const_iterator col_it;
+
+	// Add each element
+	for (col_it = row->begin(); col_it != row->end(); ++col_it) {
+		;
+	}
+}
+
+// vi: set tabstop=4 softtabstop=4 shiftwidth=4 smarttab noexpandtab:
