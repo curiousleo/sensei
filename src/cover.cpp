21a592c8ba803dfc01a5212040e1c1bb48b42fd3
curiousleo <curiousleo@ymail.com>
2011-08-31 19:55:40 +0100

    cover.cpp: Completed add_rows; added init_columns, add_row, link_node

--- src/cover.cpp
+++ src/cover.cpp
@@ -5,23 +5,106 @@
 ExactCover::ExactCover() {};
 
 void ExactCover::add_rows(const std::vector<std::vector<bool> > *rows) {
-	int row_id = 0;
-	std::vector<std::vector<bool> >::const_iterator row_it;
+	std::vector<std::vector<bool> >::size_type row_i;
+
+	// Add each column
+	init_columns(rows->size());
 	
 	// Add each row
-	for (row_it = rows->begin(); row_it != rows->end(); ++row_it) {
-		add_row(&(*row_it), row_id);
-		++row_id;
+	for (row_i = 0; row_i != rows->size(); ++row_i) {
+		add_row(&(*rows)[row_i], row_i);
 	}
 }
 
-void ExactCover::add_row(const std::vector<bool> *row, int row_id) {
-	std::vector<bool>::const_iterator col_it;
+void ExactCover::init_columns(const int col_count) {
+	// Reserve space for all the columns and the root column
+	columns.reserve(col_count + 1);
 
-	// Add each element
-	for (col_it = row->begin(); col_it != row->end(); ++col_it) {
-		;
+	// Create root column
+	ExactCoverColumn *root = new ExactCoverColumn;
+	root->index = 0;
+	root->head.tag = 0;
+
+	columns.push_back(root);
+
+	for (int col_i = 0; col_i != col_count; ++col_i) {
+		ExactCoverColumn *column = new ExactCoverColumn;
+
+		column->index = col_i + 1;
+
+		// Initialize empty column
+		column->size = 0;
+		column->head.tag = -1; // ???
+
+		// Initialize column head
+		column->head.up = &(column->head);
+		column->head.down = &(column->head);
+
+		root->prev = column; // Can be optimized
+		columns.back()->next = column;
+
+		column->prev = columns.back();
+		column->next = root;
+
+		columns.push_back(column);
 	}
+}
+
+void ExactCover::add_row(const std::vector<bool> *row, const int row_i) {
+	std::vector<bool>::size_type col_i;
+	ExactCoverNode *row_start = NULL;
+
+	// Add each element (node)
+	for (col_i = 0; col_i != row->size(); ++col_i) {
+		if ((*row)[col_i]) {
+			link_node(row_i, col_i, &row_start);
+		}
+	}
+}
+
+void ExactCover::link_node(const int row_i, const int col_i, ExactCoverNode **row_start) {
+	// Goal: Populate all node members: left, right, up, down, column, tag
+	
+	ExactCoverNode *node = new ExactCoverNode;
+	nodes.push_back(node);
+
+	// Populate tag and column fields
+	node->tag = row_i;
+	node->column = columns[col_i];
+
+	// Set row_start to node if row is the first node in that column
+	if (*row_start == NULL) *row_start = node;
+
+	// Populate down field
+	node->down = &(columns[col_i]->head);
+	columns[col_i]->head.up = node;
+
+	// Populate up field
+	// Is node the only element in that column?
+	if (columns[col_i]->size == 0) {
+		node->up = &(columns[col_i]->head);
+		columns[col_i]->head.down = node;
+	} else {
+		// Insert as last element in that column
+		node->up = columns[col_i]->head.up;
+		columns[col_i]->head.up->down = node;
+	}
+
+	// Populate left and right fields
+	if (*row_start != node) {
+		// Insert as last element in that row
+		node->left = (*row_start)->left;
+		node->right = (*row_start);
+
+		(*row_start)->left->right = node;
+		(*row_start)->left = node;
+	} else {
+		// Insert as first and only element in that row
+		node->left = node;
+		node->right = node;
+	}
+
+	columns[col_i]->size++;
 }
 
 // vi: set tabstop=4 softtabstop=4 shiftwidth=4 smarttab noexpandtab:
