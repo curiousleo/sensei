54d6d02d044f64151e1d88fb284b8f8c88495b95
curiousleo <curiousleo@ymail.com>
2011-09-03 17:48:57 +0100

    cover.cpp, cover.hpp: Changed all references to call-by-value

--- src/cover.cpp
+++ src/cover.cpp
@@ -8,7 +8,7 @@ ExactCover::ExactCover() {
 	cur_column = NULL;
 }
 
-ExactCover::ExactCover(const std::list<std::list<int> > &rows) {
+ExactCover::ExactCover(const std::list<std::list<int> > rows) {
 	cur_node = NULL;
 	cur_column = NULL;
 
@@ -124,7 +124,7 @@ ExactCoverColumn *ExactCover::smallest_column() {
 	return min_column;
 }
 
-void ExactCover::cover_row(ExactCoverNode &row) {
+void ExactCover::cover_row(ExactCoverNode row) {
 	// Cover each column linked to this row
 	for (
 			ExactCoverNode *node = row.right;
@@ -133,7 +133,7 @@ void ExactCover::cover_row(ExactCoverNode &row) {
 	}
 }
 
-void ExactCover::uncover_row(ExactCoverNode &row) {
+void ExactCover::uncover_row(ExactCoverNode row) {
 	// Uncover each column linked to this row
 	for (
 			ExactCoverNode *node = row.right;
@@ -142,7 +142,7 @@ void ExactCover::uncover_row(ExactCoverNode &row) {
 	}
 }
 
-void ExactCover::cover_column(ExactCoverColumn &column) {
+void ExactCover::cover_column(ExactCoverColumn column) {
 	// Unlink column from the column list
 	column.prev->next = column.next;
 	column.next->prev = column.prev;
@@ -168,7 +168,7 @@ void ExactCover::cover_column(ExactCoverColumn &column) {
 	}
 }
 
-void ExactCover::uncover_column(ExactCoverColumn &column) {
+void ExactCover::uncover_column(ExactCoverColumn column) {
 	// Re-insert column into column list
 	column.prev->next = column.next->prev = &column;
 	
@@ -192,7 +192,7 @@ void ExactCover::uncover_column(ExactCoverColumn &column) {
 	}
 }
 
-void ExactCover::set_rows(const std::list<std::list<int> > &rows) {
+void ExactCover::set_rows(const std::list<std::list<int> > rows) {
 	std::list<std::list<int> >::const_iterator row_it;
 
 	// Clear columns & rows
@@ -253,7 +253,7 @@ void ExactCover::init_columns(const int col_count) {
 	}
 }
 
-void ExactCover::add_row(const std::list<int> &row, const int row_i) {
+void ExactCover::add_row(const std::list<int> row, const int row_i) {
 	ExactCoverNode *row_start = NULL;
 
 	// Add each element (node)
