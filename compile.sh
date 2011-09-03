50fa0d2bcb68e99e341733e85b5ca8f63a969fe2
curiousleo <curiousleo@ymail.com>
2011-09-03 17:51:43 +0100

    compile.sh: gcc -> g++

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-CMD="gcc-4.4 -std=c++0x -O3 -o bin/sensei src/cover.cpp src/sensei.cpp"
+CMD="g++-4.4 -std=c++0x -O3 -o bin/sensei src/cover.cpp src/sensei.cpp"
 echo "Compiling with command: $CMD"
 $($CMD)
 chmod +x bin/sensei
