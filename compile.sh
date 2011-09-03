06fab81c971b1a5571253b7f8be91ca119b967cc
curiousleo <curiousleo@ymail.com>
2011-09-03 16:56:51 +0100

    compile.sh: Removed '-lboost_thread-mt' option

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-CMD="gcc-4.4 -std=c++0x -lboost_thread-mt -O3 -o bin/sensei src/cover.cpp src/sensei.cpp"
+CMD="gcc-4.4 -std=c++0x -O3 -o bin/sensei src/cover.cpp src/sensei.cpp"
 echo "Compiling with command: $CMD"
 $($CMD)
 chmod +x bin/sensei
